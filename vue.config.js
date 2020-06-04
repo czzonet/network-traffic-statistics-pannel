16c3706056f81cd105674995a9276965c212d7e5
czzonet <czzonet@163.com>
2020-06-04 16:42:17 +0800

    [feat] vue with cdn vue使用cdn加速

--- vue.config.js
+++ vue.config.js
@@ -31,6 +31,19 @@ module.exports = {
   },
   configureWebpack: {
     devtool: "source-map",
+    externals: {
+      /**
+       * 这里采用Object的形式
+       * 更多的形式参考(https://webpack.js.org/configuration/externals/#src/components/Sidebar/Sidebar.jsx)
+       * key: 依赖包的名字
+       * value: 依赖包挂载在项目中的变量名
+       * 挂载的变量必须与依赖包中挂载的一样，在项目中使用也是这个变量名
+       */
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      axios: "axios",
+    },
   },
   // chainWebpack: (config) => {
   //   // new Loader for ts
