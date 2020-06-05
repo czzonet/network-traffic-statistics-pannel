0fd81bfe2077496991555f4953414302638dbf1f
czzonet <czzonet@163.com>
2020-06-05 13:23:39 +0800

    [faet] elementui component elementui 按需引入

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import router from "./router";
 import store from "./store";
 
 /** element ui */
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
-Vue.use(ElementUI);
+import { initElementUI } from "./elementui-components-init";
+initElementUI();
 
 Vue.config.productionTip = false;
 
